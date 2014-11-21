1759b60734fcea443b3af19636c0f26748aa27f9
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2014-11-21 23:08:29 +0100

    multi line command

--- configure.bat
+++ configure.bat
@@ -1,1 +1,8 @@
-waf\waf configure --boost-include=D:\boost_1_57_0 --boost-lib=D:\boost_1_57_0\lib32-msvc-11.0 --boost-mt --opencv-lib=D:\opencv\build\x86\vc11\staticlib --opencv-include=D:\opencv\build\include --opencv-ver=2410 %*
+waf\waf configure^
+  --boost-include=D:\boost_1_57_0^
+  --boost-lib=D:\boost_1_57_0\lib32-msvc-11.0^
+  --boost-mt^
+  --opencv-lib=D:\opencv\build\x86\vc11\staticlib^
+  --opencv-include=D:\opencv\build\include^
+  --opencv-ver=249^
+  %*
