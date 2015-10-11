88c306ca075fa0499e798b3e8a7fa5dc3e052ec6
Øyvind Saltvik <oyvind.saltvik@gmail.com>
2015-10-11 03:39:37 +0200

    remove unrelated commands

--- bootstrap.bat
+++ bootstrap.bat
@@ -2,17 +2,6 @@ python get_waf.py
 
 call variables.bat
 
-git clone https://github.com/cpp-netlib/cpp-netlib.git
-cd cpp-netlib
-git submodule init
-git submodule update
-cd ..
-mkdir cpp-netlib-build
-cd cpp-netlib-build
-cmake ..\cpp-netlib -G "Visual Studio 14 2015 Win64"
-cmake --build . --config Release
-cd ..
-
 git clone https://github.com/Itseez/opencv.git opencv_sources
 mkdir opencv_build
 cd opencv_build
